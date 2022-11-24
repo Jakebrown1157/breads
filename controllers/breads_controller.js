64bdaa745a11dfed2cbe79bc58c42d9afe39ece0
jake <jakebrown536@gmail.com>
2022-11-23 19:33:17 -0700

    fixed the stub pages and the internal orginization

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,14 +2,28 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('Index', {
+      breads: Bread
+    }
+  )
+// res.send(Bread)
 })
+
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.show('404')
+  }
 })
+
   
 
 module.exports = breads
--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT
 
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+
+//routes
 app.get('/', (req,res)=> {
     res.send('welcome to an Awesome App about breads!')
 })
@@ -12,6 +20,13 @@ app.get('/', (req,res)=> {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads',breadsController)
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
+  
+
+//listen
 app.listen(PORT, () => {
     console.log('listening on port' , PORT)
 })
